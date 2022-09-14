3f2e3dde8c491b6ea360665739e9824adb4dfe87
Ahmadjon Azamjonov <109953832+azamjonov07@users.noreply.github.com>
2022-09-14 12:55:33 +0500

    Add files via upload

---
+++ Homework-1 3-Month/script.js
@@ -0,0 +1,279 @@
+///1-Problem///
+
+// var number = 1111
+// if (number > 0 ){
+//     console.log('Berilgan son musbat son')
+// }else{
+//     console.log('Berilgan son musbat emas!')
+// }
+
+///2-Problem///
+
+// var number = 1111
+// if (number % 2 == 1 ){
+//     console.log('Berilgan son toq son')
+// }else{
+//     console.log('Berilgan son juft son')
+// }
+
+///3-Problem///
+
+// var number = 1110
+// if (number % 2 == 0 ){
+//     console.log('Berilgan son juft son')
+// }else{
+//     console.log('Berilgan son toq son')
+// }
+
+///4-Problem///
+
+// var first = 6
+// var second = 6
+// if ( first > 0 && second > 0){
+//     console.log('Berilgan masala togri')
+// }else{
+//     console.log('Berilgan masala xato')
+// }
+
+///5-Problem///
+
+// var numberA = 7
+// var numberB = 6
+// var numberC = 5
+// if (numberA > numberB && numberB > numberC ){
+//     console.log('Berilgan masala togri')
+// }else{
+//     console.log('Berilgan masala xato')
+// }
+
+///6-Problem///
+
+// var numberA = 3
+// var numberB = 4
+// var numberC = 5
+// if (numberA < numberB && numberB < numberC ){
+//     console.log('Berilgan masala togri')
+// }else{
+//     console.log('Berilgan masala xato')
+// }
+
+///7-Problem///
+
+// var number1 = 55
+// var number2 = 77
+// if (number1 % 2 == 1 && number2 % 2 ==1 ){
+//     console.log('Berilgan sonlar toq son')
+// }else{
+//     console.log('Berilgan sonlar juft son')
+// }
+
+///8-Problem///
+
+// var number1 = 55
+// var number2 = 77
+// var number3 = 22
+// var number4 = 66
+// if (number1 > 0 && number2 >0 && number3 > 0 && number4 >0 ){
+//     console.log('Berilgan sonlar musbat sonlar')
+// }else{
+//     console.log('Berilgan sonlar manfiy sonlar')
+// }
+
+///9-Problem///
+
+// var number1 = 55
+// var number2 = 77
+// var number3 = -22
+// if (number1 > 0 && number2 >0 && number3 < 0 ){
+//     console.log('Berilgan masala togri')
+// }else{
+//     console.log('Berilgan masala xato')
+// }
+
+///10-Problem///
+  
+
+// var number = prompt("Which week day ")
+// if (number == 1){
+//     console.log('Monday')
+// }if (number == 2){
+//     console.log('Tuesday')
+// }if (number == 3){
+//     console.log('Wednesday')
+// }if (number == 4){
+//     console.log('Thirsday')
+// }if (number == 5){
+//     console.log('Friday')
+// }if (number == 6){
+//     console.log('Saturday')
+// }if (number == 7){
+//     console.log('Sunday')
+// }else if (number > 7){
+//     console.log('There is no such day !');
+// }
+
+// ///11-Problem///
+
+// var number = prompt("")
+// if (number == 94 ){
+//     console.log('Usel')
+// }else{
+//     console.log('Xato');
+// }
+// if (number == 93){
+//     console.log('Usel')
+// }if (number == 95){
+//     console.log('Uzmobile')
+// }if (number == 90){
+//     console.log('Beeline')
+// }if (number == 97){
+//     console.log('Mobi Uz')
+// }if (number == 91){
+//     console.log('Beeline')
+// }if (number == 71){
+//     console.log('Gorodskoy')
+// }if (number == 33){
+//     console.log('Hyumans')
+// }
+///12-Problem///
+
+// var number = -8
+// if (number > 0){
+//     console.log (number +1)
+// }else{
+//     console.log (number +1)
+// }
+
+///13-Problem///
+
+// var number = -8
+// if (number > 0){
+//     console.log (number +3)
+// }else{
+//     console.log (number +2)
+// }
+
+
+///14-Problem///
+
+// var number1 =7
+// var number2 =5
+// if (number1 > number2){
+//     console.log(number1)
+// }else{
+//     console.log(number2)
+// }
+
+///15-Problem///
+
+// var number1 =9
+// var number2 =8
+// var number3 =7
+// if (number1 > number2 && number1 > number3){
+//     console.log(number1)
+// }else if(number2 >  number1 && number2 >number3){
+//     console.log(number2)
+// }if(number3 > number1 && number3 > number2){
+//      console.log(number3)
+//  }
+
+///16-Problem///
+
+// var number1 =9
+// var number2 =8
+// var number3 =7
+// if (number1 < number2 && number1 < number3){
+//     console.log(number1)
+// }else if(number2 <  number1 && number2 < number3){
+//     console.log(number2)
+// }if(number3 < number1 && number3 < number2){
+//      console.log(number3)
+//  }
+
+///17-Problem///
+
+
+
+
+///18-Problem///
+
+// var baho = prompt("Bahoyingiz ni kiriting 1-10")
+// if (baho > 8 && baho <11){
+//     console.log("Sizga 500.000 so'm pul ajratildi")
+// }else if(baho >6 && baho < 9){
+//     console.log("Sizga 300.000 so'm pul ajratildi")
+// }else if(baho >4 && baho < 7){
+//     console.log("Sizga 100.000 so'm pul ajratildi")
+// }else if(baho > 0 && baho < 5){
+//     console.log("Sizga pul yoq, yahshiroq oqing!")
+// }else if(baho >10 ){
+//     console.log("Bunday baho yoq, qayta urinib koring!")
+// }
+
+///19-Problem///
+
+// var number = prompt("Hohlagansoningiz ni kiriting")
+// if(number % 2 ==1 && number > 0){
+//     console.log('Bu son - toq musbat son');
+// }else if (number % -2 ==-1 && number < 0){
+// console.log('Bu son - toq manfiy son');
+// }else if(number % 2 ==0 && number > 0){
+//     console.log('Bu son - juft musbat son');
+// }else if (number % -2 ==0 && number < 0){
+// console.log('Bu son - juft manfiy son');
+// }
+
+///20-Problem///
+
+// var yil = prompt()
+// if(  yil < 2023){
+//     console.log(2022- yil)
+// }else{
+//     console.log('Bunday yil hali kemagan')
+// }
+
+///21-Problem///
+
+// var number1 = 6
+// var number2 = 5
+// var number3 = 5
+// if(number1>= number2 && number2>= number3){
+//     console.log("Masala tog'ri")
+// }else{
+//     console.log("masala notog'ri")
+// }
+
+///22-Problem///
+
+// var number1 = 11
+// var number2 = 10
+// if( number1 % 2 ==0 && number2 %2 ==1){
+//     console.log("Masala tog'ri")
+// }else if( number1 % 2 ==1 && number2 %2 ==0){
+//    console.log("Masala tog'ri");
+// }else{
+//     console.log("Masala notog'ri");
+// }
+
+///23-Problem///
+
+// var numberA = 5
+// var numberB = 5
+// var numberC = 5
+// if(numberA > 0 && numberB > 0 && numberC > 0 ){
+//     console.log("Masala tog'ri");
+// }else{
+//     console.log("Masala no'togri");
+// }
+
+///24-Problem///
+
+
+var numberA = 5
+var numberB = 5
+var numberC = 5
+if(numberA < 0 && numberB < 0 && numberC < 0 ){
+    console.log("Masala tog'ri");
+}else{
+    console.log("Masala no'togri");
+}
